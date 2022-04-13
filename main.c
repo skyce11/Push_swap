0b2a13ad1d8bd367e5abcbea56982622acc2c5d3
skyce11 <89009437+skyce11@users.noreply.github.com>
2022-04-13 06:44:04 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -6,16 +6,11 @@
 /*   By: migonzal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 08:16:39 by migonzal          #+#    #+#             */
-/*   Updated: 2022/03/25 10:22:24 by migonzal         ###   ########.fr       */
+/*   Updated: 2022/04/13 08:36:43 by migonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-
-
-
-
 
 
 void	get_pos(t_list **list, int pos)
@@ -45,7 +40,6 @@ void	get_pos(t_list **list, int pos)
 		sig = sig -> next;
 	}
 	aux -> pos = pos;
-
 }
 
 void	ft_setposition(t_list **list)
@@ -70,7 +64,8 @@ void	ft_setposition(t_list **list)
 int main(int argc, char **argv)
 {
 	t_list *a = 0;
-	char **s = malloc (1000000);
+	t_list *b = 0;
+	char **s = malloc (sizeof(char *) * argc);
 
 
 	int i;
@@ -92,36 +87,69 @@ int main(int argc, char **argv)
 	char *sp = " ";
 
 
+//	free(s1);
 	while (argv[++i])
 	{
+	/*	while(argv[i][++j])
+		{
+			if(ft_isdigit(argv[i][j]) == 0 && argv[i][j] != 32)
+			{
+				printf("Aqui hay cosas que no son mumeros");
+				return (0);
+			}
+
+
+		}*/
+	//	j = -1;
+		free(s1);
 		s1 = ft_strjoin(s1, argv[i]);
+		free(s1);
 		s1 = ft_strjoin(s1, sp);
 	}
+//	free(sp);
 
-	printf("Valor del array %s\n", s1);
 	
 	i = -1;
-
+//aqui nop
+	free(s);
 	s = ft_split(s1, ' ');
+	free(s1);
 
+	//system("leaks push_swap");	
 	while (s[++i])
 	{
 		ft_lstadd_back(&a, ft_lstnew(ft_atoi(s[i])));
+	}
+	free_list(s);
 
+	if (ft_repeated(a) == 0)
+	{
+		printf("ERROR, mio\n");
+		return (0);
 	}
 
-
-
 	ft_setposition(&a);
-	int contador = ft_lstsize(a);
-	printf("Elementos de la lista %d\n", contador);
-	ft_lstiter(a);
-	printf("---------------------------------\n");
-	ft_rra(&a);
-	ft_lstiter(a);
+	if (ordered(a))
+	{
 
+		printf("TODO CORRECTO");
+		free_variables(&a);
+		return (0);
+	}
+	leer(a, b);
+	if (ft_lstsize(a) <= 3)
+		ft_distribution(&a, &b);
+	else if (ft_lstsize(a) == 4)
+		ft_stack4(&a, &b);
+	else if (ft_lstsize(a) == 5)
+		ft_stack5(&a, &b);
+	else
+		ft_algorithm(&a, &b);
+	leer(a, b);
+	free_variables(&a);
+	free_variables(&b);
+//	system("leaks push_swap");
 
-	
 	return (0);
 
 }
--- push_swap.h
+++ push_swap.h
@@ -6,9 +6,12 @@
 /*   By: migonzal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 10:00:23 by migonzal          #+#    #+#             */
-/*   Updated: 2022/03/24 13:36:43 by migonzal         ###   ########.fr       */
+/*   Updated: 2022/04/13 08:17:44 by migonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
 #include <stdio.h>
 #include <unistd.h>
@@ -29,15 +32,59 @@ t_list *ft_lstnewpos(int num, int pos);
 void	*ft_lstadd_back(t_list **lst, t_list *new);
 void	*ft_lstadd_front(t_list **list, t_list *new);
 void	*ft_lstclear(t_list **list, void (*del) (void *));
-void	ft_lstdelone(t_list **lst);
+void	ft_lstdelone(t_list *lst);
 void	ft_lstiter(t_list *lst);
 t_list *ft_lstlast(t_list *lst);
 t_list *ft_map(t_list *lst, void *(*f) (void *), void(*del) (void *));
 int	ft_lstsize(t_list *list);
 
+/* Auxs movs <= 5*/
+
+void	ft_or4pos0(t_list **a, t_list **b);
+void	ft_or4pos3(t_list **a, t_list **b);
+void	ft_or4pos1(t_list **a, t_list **b);
+void	ft_or4pos2(t_list **a, t_list **b);
+/*Movimientos <= 5 */
+
+void	ft_stack5(t_list **a, t_list **b);
+void	ft_stack4(t_list **a, t_list **b);
+void	ft_distribution(t_list **a, t_list **b);
+int		ft_getmax(t_list *a);
+int		ft_getmin(t_list *a);
+int		ft_getmaxpos(t_list *a, int n);
+int		ft_getminpos(t_list *a, int n);
+void	ft_132(t_list **a, t_list **b);
+void	ft_231(t_list **a);
+void	ft_213(t_list **a);
+void	ft_321(t_list **a, t_list **b);
+void	ft_312(t_list **a);
+
+/*Auxiliares */
+
+int		ft_repeated(t_list *a);
+void	free_variables(t_list **list);
+void	free_list(char **list);
+void	leer(t_list *a, t_list *b);
+int		ordered(t_list *a);
+int		ft_pow(int num);
+void	ft_algorithm(t_list **a, t_list **b);
+//void	get_pos(t_list **list, int pos);
+//void	ft_setposition(t_list **list);
+
 
 /* Movimientos */
+void	ft_pa(t_list **a, t_list **b);;
+void	ft_pb(t_list **a, t_list **b);
 void	ft_ra(t_list **a);
 void	ft_rra(t_list **a);
+int		ft_rra_aux(t_list **a);
 void	ft_rb(t_list **b);
 void	ft_sa(t_list **a);
+void	ft_sb(t_list **b);
+void	ft_ss(t_list **a, t_list **b);
+void	ft_rrb(t_list **b);
+int		ft_rrb_aux(t_list **b);
+void	ft_rr(t_list **a, t_list **b);
+void	ft_rrr(t_list **a, t_list **b);
+
+#endif
